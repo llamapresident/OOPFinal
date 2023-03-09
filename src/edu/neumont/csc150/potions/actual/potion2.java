9da1adeb005ff3751d18ac352e3fb60ee45dad69
llamapresident <47728115+llamapresident@users.noreply.github.com>
2023-03-09 03:24:26 -0700

    finishing touches(for now)

--- src/edu/neumont/csc150/potions/actual/potion2.java
+++ src/edu/neumont/csc150/potions/actual/potion2.java
@@ -3,5 +3,4 @@ package edu.neumont.csc150.potions.actual;
 public abstract class potion2 extends potion{
     Ingredients ingredient2;
     public abstract void Ingredient2(String ingredients);
-    public abstract void setEffect();
 }
